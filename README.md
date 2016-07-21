9a2ca976e419c8d463fa538ba3ac609aef2fc7d5
aereal <aereal@aereal.org>
2016-07-21 11:43:59 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::merge\_cpanfile - It's new $module
+
+# SYNOPSIS
+
+    use App::merge_cpanfile;
+
+# DESCRIPTION
+
+App::merge\_cpanfile is ...
+
+# LICENSE
+
+Copyright (C) aereal.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+aereal <aereal@aereal.org>
---
+++ lib/App/merge_cpanfile.pm
@@ -0,0 +1,39 @@
+package App::merge_cpanfile;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::merge_cpanfile - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::merge_cpanfile;
+
+=head1 DESCRIPTION
+
+App::merge_cpanfile is ...
+
+=head1 LICENSE
+
+Copyright (C) aereal.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+aereal E<lt>aereal@aereal.orgE<gt>
+
+=cut
+
