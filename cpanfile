599383106c924705c2a67e6dacc640eca1dce724
aereal <aereal@aereal.org>
2016-07-21 11:53:03 +0900

    + merge-cpanfile

--- cpanfile
+++ cpanfile
@@ -1,5 +1,8 @@
 requires 'perl', '5.008001';
 
+requires 'CPAN::Meta::Prereqs';
+requires 'Module::CPANfile';
+
 on 'test' => sub {
     requires 'Test::More', '0.98';
 };
--- lib/App/merge_cpanfile.pm
+++ lib/App/merge_cpanfile.pm
@@ -3,9 +3,46 @@ use 5.008001;
 use strict;
 use warnings;
 
+use Module::CPANfile;
+use CPAN::Meta::Prereqs;
+
 our $VERSION = "0.01";
 
+sub run {
+  my ($self, @argv) = @_;
+  my ($dest, $src) = @argv;
 
+  my $dest_cpanfile = Module::CPANfile->load($dest);
+  my $src_cpanfile = Module::CPANfile->load($src);
+
+  my $merged_cpanfile = $self->merge_cpanfiles(
+    $src_cpanfile,
+    $dest_cpanfile,
+    'runtime',
+    'requires',
+  );
+
+  print $merged_cpanfile->to_string;
+}
+
+sub merge_cpanfiles {
+  my ($self, $src, $dest, $phase, $type) = @_;
+
+  my $merged_prereqs = $dest->prereqs->with_merged_prereqs(
+      $self->extract_prereqs_for($src->prereqs, $phase, $type)
+  );
+
+  return Module::CPANfile->from_prereqs($merged_prereqs->as_string_hash);
+}
+
+sub extract_prereqs_for {
+    my ($self, $prereqs, $phase, $type) = @_;
+    my $reqs = $prereqs->requirements_for($phase, $type);
+    my $new_prereqs = CPAN::Meta::Prereqs->new({
+        $phase => { $type => $reqs->as_string_hash }
+    });
+    return $new_prereqs;
+}
 
 1;
 __END__
